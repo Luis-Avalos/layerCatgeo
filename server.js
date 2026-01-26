ddf344ac904e9300d53b31bcdeabe546f26a0dca
Luis-avalos <Luis.avalos.luevano@gmail.com>
2026-01-26 09:54:04 -0600

    change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 import app from "./src/app.js";
 
-const PORT = 443;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
